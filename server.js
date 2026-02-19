482a9384278158235030e43fc7da83fa7df68e9b
junzvb <junaidvbergfarm@gmail.com>
2026-02-19 22:37:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+require("./scheduler");
 
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Trading Alert Bot Running");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
+});
